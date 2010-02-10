3e8bdf7a081b3cdd54cc6df88ad7f78ec31dd632
Niko Dittmann <niko@restorm.com>
2010-02-10 09:06:15 +0100

    . now using rack/test (as sinatra/test is depricated)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require 'shoulda'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'sinatra/base'
-require 'sinatra/test'
+require 'rack/test'
 require 'sinatra_named_routes'
 
 class SinatraNamedRoutesTestApp < Sinatra::Base
